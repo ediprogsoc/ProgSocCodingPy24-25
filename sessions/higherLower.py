47b7326e043151e8633561ccb0988b273fee9aa3
Igi-Gudel <145386842+Igi-Gudel@users.noreply.github.com>
2024-10-01 11:39:46 +0100

    Made a quick higher or lower game.

---
+++ sessions/higherLower.py
@@ -0,0 +1,23 @@
+import random
+
+low: int = 1
+high: int = 10
+nums: list[int] = []
+nums.append(random.randint(low, high))
+score: int = 0
+running: bool = True
+while running:
+    nums.append(random.randint(low, high))
+    print(f"Higher or lower (H/L) than {nums[-2]}. (Range of {low} to {high})")
+    playerInput: str = input().upper()
+    if playerInput == "H" and nums[-2] <= nums[-1]:
+        score += 1
+        print(nums[-1], "was higher than", nums[-2])
+    elif playerInput == "L" and nums[-2] >= nums[-1]:
+        score += 1
+        print(nums[-1], "was lower than", nums[-2])
+    else:
+        running = False
+    high += 1
+print("The number was", nums[-2])
+print("Your Score is", score)
